c0b244c93206e92bf869fdeeedc69234cd7552f7
Abhishekgowda <abhishek@emproto.com>
2022-03-01 20:52:49 +0530

    initial commit

---
+++ src/test/java/com/abhishek/springsseemitter/SpringSseEmitterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abhishek.springsseemitter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSseEmitterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
